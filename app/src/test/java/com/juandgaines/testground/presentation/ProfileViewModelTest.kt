23c0772a3e5a4920c02ad2d838d501374ea41ac4
LDHugeman <luisdavid.vp@gmail.com>
2025-05-27 19:11:44 +0200

    clase 8

--- app/src/test/java/com/juandgaines/testground/presentation/ProfileViewModelTest.kt
+++ app/src/test/java/com/juandgaines/testground/presentation/ProfileViewModelTest.kt
@@ -1,34 +1,30 @@
 package com.juandgaines.testground.presentation
 
 import androidx.lifecycle.SavedStateHandle
-import app.cash.turbine.test
-import com.google.common.truth.Truth.assertThat
-import com.juandgaines.testground.domain.Coordinates
-import com.juandgaines.testground.domain.Place
-import com.juandgaines.testground.domain.Profile
-import com.juandgaines.testground.domain.User
-import com.juandgaines.testground.domain.UserRepository
-import com.juandgaines.testground.util.MainDispatcherRule
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import com.google.common.truth.Truth
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
 import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
-import java.util.UUID
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class ProfileViewModelTest {
 
-    @get:Rule
-    val mainDispatcherRule = MainDispatcherRule()
-
+    private val testDispatcher = UnconfinedTestDispatcher()
     private lateinit var viewModel: ProfileViewModel
     private lateinit var repository: UserRepositoryFake
 
     @Before
     fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+
         repository = UserRepositoryFake()
+
         viewModel = ProfileViewModel(
             repository = repository,
             savedStateHandle = SavedStateHandle(
@@ -39,15 +35,19 @@ class ProfileViewModelTest {
         )
     }
 
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+    }
+
     @Test
     fun givenValidUserId_whenLoadProfile_thenProfileIsLoaded() = runTest {
         // Act
         viewModel.loadProfile()
-        advanceUntilIdle()
 
         // Assert
-        assertThat(viewModel.state.value.profile).isEqualTo(repository.profileToReturn)
-        assertThat(viewModel.state.value.isLoading).isFalse()
+        Truth.assertThat(viewModel.state.value.profile).isEqualTo(repository.profileToReturn)
+        Truth.assertThat(viewModel.state.value.isLoading).isFalse()
     }
 
     @Test
@@ -57,69 +57,10 @@ class ProfileViewModelTest {
 
         // Act
         viewModel.loadProfile()
-        advanceUntilIdle()
 
         // Assert
-        assertThat(viewModel.state.value.profile).isNull()
-        assertThat(viewModel.state.value.errorMessage).isEqualTo("Test exception")
-        assertThat(viewModel.state.value.isLoading).isFalse()
-    }
-
-    @Test
-    fun givenLoadingState_whenLoadProfile_thenStateUpdatesCorrectly() = runTest {
-        // Act & Assert
-        viewModel.state.test {
-            val emission1 = awaitItem()
-            assertThat(emission1.isLoading).isFalse()
-
-            viewModel.loadProfile()
-
-            val emission2 = awaitItem()
-            assertThat(emission2.isLoading).isTrue()
-
-            val emission3 = awaitItem()
-            assertThat(emission3.isLoading).isFalse()
-            assertThat(emission3.profile).isEqualTo(repository.profileToReturn)
-        }
+        Truth.assertThat(viewModel.state.value.profile).isNull()
+        Truth.assertThat(viewModel.state.value.errorMessage).isEqualTo("Test exception")
+        Truth.assertThat(viewModel.state.value.isLoading).isFalse()
     }
 }
-
-class UserRepositoryFake : UserRepository {
-    var profileToReturn = profile()
-    var errorToReturn: Exception? = null
-
-    override suspend fun getProfile(userId: String): Result<Profile> {
-        return if (errorToReturn != null) {
-            Result.failure(errorToReturn!!)
-        } else Result.success(profileToReturn)
-    }
-
-    override suspend fun getPlaces(userId: String): Result<List<Place>> {
-        return Result.success(profileToReturn.places)
-    }
-}
-
-private fun user(): User {
-    return User(
-        id = UUID.randomUUID().toString(),
-        username = "test-user"
-    )
-}
-
-private fun place(userId: String): Place {
-    return Place(
-        id = UUID.randomUUID().toString(),
-        name = "test place",
-        coordinates = Coordinates(1.0, 1.0)
-    )
-}
-
-private fun profile(): Profile {
-    val user = user()
-    return Profile(
-        user = user,
-        places = (1..10).map {
-            place(user.id)
-        }
-    )
-} 
